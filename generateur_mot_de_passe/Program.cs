29a671b001f40e822bca9cee1409de32b9338ff1
Caroline Owen <carolinaowen95@gmail.com>
2022-10-13 19:04:29 +0200

    class_outil

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -1,60 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using formationC;
 using System;
 
 namespace generateur_de_mot_de_passe // Note: actual namespace depends on the project name.
 {
     class Program
     {
-        static int DemanderNombrePositifNonNul(string question)
-        {
-
-           
-                int nombre = DemanderNombre(question);
-                if (nombre > 0)
-                {
-                    return nombre;
-                }
-                Console.WriteLine($"Le nombre doit être supérieur à 0 ");
-            return DemanderNombrePositifNonNul(question);
-        }
-        static int DemanderNombreEntre(string question, int min, int max)
-        {
-            while (true)
-            {
-                int nombre = DemanderNombre(question);
-                if ((nombre >= min) && (nombre <= max))
-                {
-                    return nombre;
-                }
-                Console.WriteLine($"Le nombre doit être compris entre {min} et {max} ");
-             
-            }
-            
-        }
-        static int DemanderNombre(string question)
-        {
-            
-            while (true)
-            {
-                Console.WriteLine(question);
-                string reponse = Console.ReadLine();
-                try
-                {
-                    int responseNum = int.Parse(reponse);
-                    return responseNum;
-                }
-                catch
-                {
-                    Console.WriteLine("Veuillez choisir un nombre au format valide");
-                }
-            
-            }
-           
-        }
+        
         static void Main(string[] args)
         {
-            int longueurMotDePasse = DemanderNombre("Quelle est la longueur du mot de passe?");
+            int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Quelle est la longueur du mot de passe?");
         }
     }
 }
--- generateur_mot_de_passe/outils.cs
+++ generateur_mot_de_passe/outils.cs
@@ -4,10 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace generateur_mot_de_passe
+namespace formationC
+
 {
     static class outils
     {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
 
+
+            int nombre = DemanderNombre(question);
+            if (nombre > 0)
+            {
+                return nombre;
+            }
+            Console.WriteLine($"Le nombre doit être supérieur à 0 ");
+            return DemanderNombrePositifNonNul(question);
+        }
+        public static int DemanderNombreEntre(string question, int min, int max)
+        {
+            while (true)
+            {
+                int nombre = DemanderNombre(question);
+                if ((nombre >= min) && (nombre <= max))
+                {
+                    return nombre;
+                }
+                Console.WriteLine($"Le nombre doit être compris entre {min} et {max} ");
+
+            }
+
+        }
+        public static int DemanderNombre(string question)
+        {
+
+            while (true)
+            {
+                Console.WriteLine(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int responseNum = int.Parse(reponse);
+                    return responseNum;
+                }
+                catch
+                {
+                    Console.WriteLine("Veuillez choisir un nombre au format valide");
+                }
+
+            }
+
+        }
     }
 }
