55cedfa33dbf17061d7006fa8528e5bf047f69f3
Caroline Owen <carolinaowen95@gmail.com>
2022-10-13 18:54:38 +0200

    Fonctions_nombre

--- generateur_mot_de_passe/Program.cs
+++ generateur_mot_de_passe/Program.cs
@@ -6,9 +6,57 @@ namespace generateur_de_mot_de_passe // Note: actual namespace depends on the pr
 {
     class Program
     {
+        static int DemanderNombrePositifNonNul(string question)
+        {
+
+           
+                int nombre = DemanderNombre(question);
+                if (nombre > 0)
+                {
+                    return nombre;
+                }
+                Console.WriteLine($"Le nombre doit être supérieur à 0 ");
+            return DemanderNombrePositifNonNul(question);
+        }
+        static int DemanderNombreEntre(string question, int min, int max)
+        {
+            while (true)
+            {
+                int nombre = DemanderNombre(question);
+                if ((nombre >= min) && (nombre <= max))
+                {
+                    return nombre;
+                }
+                Console.WriteLine($"Le nombre doit être compris entre {min} et {max} ");
+             
+            }
+            
+        }
+        static int DemanderNombre(string question)
+        {
+            
+            while (true)
+            {
+                Console.WriteLine(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int responseNum = int.Parse(reponse);
+                    return responseNum;
+                }
+                catch
+                {
+                    Console.WriteLine("Veuillez choisir un nombre au format valide");
+                }
+            
+            }
+           
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int longueurMotDePasse = DemanderNombre("Quelle est la longueur du mot de passe?");
         }
     }
 }
+
+
